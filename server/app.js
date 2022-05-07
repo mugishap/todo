d8d75d98db087a099cf357bc42ff1d35c7131d15
mugishap <precieuxmugisha@gmail.com>
2022-05-07 18:29:24 +0200

    commits

--- server/app.js
+++ server/app.js
@@ -2,5 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
+const mongoose = require('mongoose')
 
 app.use(bodyParser.json())
+
+const dbConnection = () => {
+ mongoose.connect   
+}
