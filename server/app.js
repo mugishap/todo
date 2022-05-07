7b5aa013813820cfbf4b78e52eec912cc20727cd
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-05-07 21:31:33 +0200

    connections

--- server/app.js
+++ server/app.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
+const dbConnection = require('./util/database')
 const app = express()
 const mongo = require('./util/database')
+const PORT = process.env.PORT || 3000
+require('dotenv').config()
 mongo();
-const router = require('../routes/routers')
 
+//db connection
+dbConnection();
+
+//server connection
+app.listen(PORT, (err, con) => {
+    if (err) {
+        console.log("failed to connect to database")
+    } else {
+        console.log("connected to database")
+    }
+})
+
+//server connection
+
+
+const router = require('../routes/routers')
 app.use("/", router);
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
