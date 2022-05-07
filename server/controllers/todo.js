403d376d6eed82c8e9d7cca16ae6b633c7ba2148
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-05-07 21:18:22 +0200

    adding a new todo

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -2,18 +2,43 @@ const express = require('express')
 const router = express.Router()
 const Todo = require('../models/todo')
 
-exports.getAllTodos = async(req , res)=>{
-   
-        const todos = await Todo.find()
-        if(!todos) return res.status(200).send('There are no todos Want to add Some')
-        res.status(200).send(todos)
-    
+exports.getAllTodos = async(req, res) => {
+
+    const todos = await Todo.find()
+    if (!todos) return res.status(200).send('There are no todos Want to add Some')
+    res.status(200).send(todos)
+
 }
 
-exports.getTodoByUsername = async(req , res)=>{
-   
+exports.getTodoByUsername = async(req, res) => {
+
     const username = req.params.username
-    const todo = await Todo.find({username:username})
-    if(!todo) return res.status(200).send(`The Todo with username ${username} was not found`)
+    const todo = await Todo.find({ username: username })
+    if (!todo) return res.status(200).send(`The Todo with username ${username} was not found`)
     res.status(200).send(todo)
 }
+
+const newTodo = async(req, res) => {
+    const { username, content } = req.body
+    const todo = new Todo({
+        username: username,
+        content: content
+    })
+    await todo.save((err, todo) => {
+        if (err) {
+            res.json({
+                status: 500,
+                message: "failed to add todo",
+                error: err.message
+            })
+        } else {
+            res.json({
+                status: 200,
+                message: "todo added successfully",
+                data: todo
+            })
+        }
+    })
+}
+
+module.exports.newTodo = newTodo
