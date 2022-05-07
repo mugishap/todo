cfe19a8f7d676565db5cb206ce4fe8c4b8b35786
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-05-07 21:18:02 +0200

    added router middleware

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
 const app = express()
 const mongo = require('./util/database')
-mongo()
+mongo();
+const router = require('../routes/routers')
 
+app.use("/", router);
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
